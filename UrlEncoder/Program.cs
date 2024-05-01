da0628c42d34c75d31fc9b73a48b59d18867538f
Hinnerk Weiler <post@hinnerk-weiler.de>
2024-05-01 10:57:50 +0200

    1.0

---
+++ UrlEncoder/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using TextCopy;
+using System.Net;
+using Spectre.Console;
+
+string input = "";
+
+if (args.Length == 0)
+{
+    AnsiConsole.MarkupLine("[red]No arguments provided, trying clipboard content[/]");
+
+    try
+    {
+        input = ClipboardService.GetText() ?? "";
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            throw new Exception("No content in clipboard");
+        }
+    }
+    catch (Exception e)
+    {
+        AnsiConsole.WriteException(e);
+        throw;
+    }
+}
+else
+{
+    input = args[0];
+    for (int i = 1; i < args.Length; i++)
+    {
+        input = $"{input} {args[i]}";
+    }
+}
+
+string encoded = WebUtility.UrlEncode(input);
+ClipboardService.SetText(encoded);
+AnsiConsole.MarkupLine($"\n\n[yellow]{encoded}[/]\n\n");
+
+AnsiConsole.MarkupLine("[gray]Encoded string copied to clipboard[/]");
---
+++ UrlEncoder/README.md
@@ -0,0 +1,11 @@
+# URL ENCODER
+
+*Very simple command line tool to encode a string to URL encoding format.*
+
+## Usage
+
+```bash
+$ urlencoder "Hello, World!"
+Hello%2C%20World%21
+```
+
